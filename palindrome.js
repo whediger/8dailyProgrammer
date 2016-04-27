4b3cd2ce593e6c25c8ec7502d17172a1614f834c
Wes Hediger <wes.hediger@gmail.com>
2016-04-27 09:59:31 -0600

    create function to check for palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,21 @@
 function alindromeCheck(stringIn){
-  
+
+  stringIn = stringIn.trim();
+  stringIn = stringIn.toLowerCase();
+  stringIn = stringIn.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  stringIn = stringIn.replace(/\s/g,'');
+
+  for (i = 0; i < stringIn.length; i++ ) {
+    var answer = null;
+    //console.log(stringIn[i]);
+    //console.log(stringIn[(stringIn.length - 1) - i]);
+    if ( stringIn[i] !== stringIn[((stringIn.length-1) - i)] ) {
+      return false;
+    } else {
+      answer = true;
+    }
+  }
+  return answer;
 }
+
+console.log(alindromeCheck("wevdvvew"));
